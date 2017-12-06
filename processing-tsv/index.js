de5172691c52bcd7a08ac3d0f8242edf1c7ae452
Daniel Teixeira <ddtxra@gmail.com>
2017-12-06 11:43:10 +0100

    adding new data

--- processing-tsv/index.js
+++ processing-tsv/index.js
@@ -1,65 +1,6 @@
 var fs = require('fs');
 var parse = require('csv-parse');
 
-function getMaxForType(blocks, type){
-    return Math.max.apply(Math,blocks.filter(g => g.type === type).map(function(o){return o.length;}))
-}
-
-function getMinForType(blocks, type){
-    return Math.min.apply(Math,blocks.filter(g => g.type === type).map(function(o){return o.length;}))
-}
-
-function getNormalizedLength(number){ 
-    //Based on histogram in R
-    //hist(log(exons$V6), breaks=5, col="lightgreen")
-    if (number < 50) 
-        return {class: "small-", length: 1} 
-    else if (number < 75) 
-        return {class: "small", length: 2} 
-    else if (number < 100) 
-        return {class: "small+", length: 3} 
-    else if (number < 125) 
-        return {class: "medium-", length: 4} 
-    else if (number < 150) 
-        return {class: "medium", length: 5} 
-    else if (number < 175) 
-        return {class: "medium+", length: 6} 
-    else if (number < 200) 
-        return {class: "large-", length: 7} 
-    else if (number < 225) 
-        return {class: "large", length: 8} 
-    else if (number < 250) 
-        return {class: "large+", length: 9} 
-    else if (number < 350) 
-        return {class: "big-", length: 10} 
-    else if (number < 1000) 
-        return {class: "big", length: 20} 
-    //Huge exons (bigger than 1000)
-    else return {class: "huge", length: 40} 
-        
-};
-
-function getNormalizedLengthForIntrons(number){ 
-    //Based on histogram in R
-    //hist(log(exons$V6), breaks=5, col="lightgreen")
-    if (number < 5000) 
-        return {class: "small-intron: The avatar can easily jump", length: 1} //simple jump --> most of them end up in this category
-    if (number < 10000) 
-        return {class: "medium-intron: need an easy spliceosome, can be small 1,2 rocks that needs to jump", length: 5} //mutiple jumps (kind of rocks) 
-    if (number < 15000) 
-        return {class: "large-intron: need a more complicated spliceosome", length: 10} //spliceosome
-    if (number < 20000) 
-        return {class: "big-intron: need a difficult spliceosome", length: 20} //Complicated spliceosome
-    return {class: "huge-intron: we need a SR protein before, like a catapult", length: 100} //SR protein (Catapult)
-};
-
-function groupBy(xs, key) {
-  return xs.reduce(function(rv, x) {
-    (rv[x[key]] = rv[x[key]] || []).push(x);
-    return rv;
-  }, {});
-};
-
 var inputFile='gene_coordinates.txt';
 var parser = parse({delimiter: '\t'}, function (err, data) {
 
@@ -80,21 +21,9 @@ var parser = parse({delimiter: '\t'}, function (err, data) {
               "ensg" : transcript[0],
               "chromosome" : line[1],
               "type" : line[6],
-              "realLength" : parseInt(line[5]),
-              "normalizedLength" : getNormalizedLength(line[5])
+              "realLength" : parseInt(line[5])
           };
-    
-            if(block.type === "intron"){
-                var normalizedIntron = getNormalizedLengthForIntrons(block.realLength);
-                block.class = normalizedIntron.class
-                block.normalizedLength = normalizedIntron.length
-            }else {
-                var normalized = getNormalizedLength(block.realLength);
-                block.class = normalized.class
-                block.normalizedLength = normalized.length
-            }
-
-            blocks.push(block);
+          blocks.push(block);
             //console.log(JSON.stringify(block));
         }
 
@@ -115,19 +44,22 @@ var parser = parse({delimiter: '\t'}, function (err, data) {
                 "transcript": block.transcript,
                 "chromosome": block.chromosome,
                 "exonsCount" : 0,
+                "exonsLength" : 0,
+                "geneLength" : 0,
                 "levelData": []
             }
             genes.push(currentGeneObject);
         }
         
-        if(block.type === "exon")
+        if(block.type === "exon") {
             currentGeneObject.exonsCount++;
-    
+            currentGeneObject.exonsLength += block.realLength;
+        }
+        currentGeneObject.geneLength += block.realLength;
+
         var level = {
             type: block.type,
             realLength: block.realLength,
-            normalizedLength : block.normalizedLength,
-            class: block.class
         }
         currentGeneObject.levelData.push(level)
 
